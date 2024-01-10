858ded25b16f73ac5f4314ffa001737952f01148
mmd <bgl.mmd@gmail.com>
2024-01-10 21:26:44 +0330

    add gitignore and README file

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -40,40 +40,40 @@ local plugins = {
   {'hrsh7th/vim-vsnip'},
 
   {"rebelot/kanagawa.nvim"},  -- neorg needs a colorscheme with treesitter support
---   {
---     "nvim-treesitter/nvim-treesitter",
---     build = ":TSUpdate",
---     opts = {
---       highlight = { enable = true },
---     },
---     config = function(_, opts)
---       require("nvim-treesitter.configs").setup(opts)
---     end,
---   },
---   {
---     "nvim-neorg/neorg",
---     build = ":Neorg sync-parsers",
---     dependencies = { "nvim-lua/plenary.nvim" },
---     config = function()
---       require("neorg").setup {
---         load = {
---           ["core.defaults"] = {},
---           ["core.concealer"] = {},
---           ["core.dirman"] = {
---             config = {
---               workspaces = {
---                 notes = "~/notes",
---               },
---               default_workspace = "notes",
---             },
---           },
---         },
---       }
--- 
---       vim.wo.foldlevel = 99
---       vim.wo.conceallevel = 2
---     end,
---   }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      highlight = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  }
 }
 
 local opts = {}
